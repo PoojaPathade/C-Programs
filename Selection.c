a38f0c2d2cbf52c904a7f2e69c71bb2dceacea25
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-05-19 15:32:14 +0530

    Selection.c

---
+++ Selection.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int No = 0;
+
+    printf("Enter the number :\n");
+    scanf("%d",&No);
+
+    if(No%2 ==0)
+    {
+        printf("It is even number...");
+    }
+    else
+    {
+        printf("It is odd number...");
+    }
+
+}
