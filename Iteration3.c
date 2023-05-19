32a6c38095decd242db971becc1b8c5f91c31ce8
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-05-19 18:22:34 +0530

    Create do while loop

---
+++ Iteration3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+    iCnt = 1;
+
+    do
+    {
+        printf("Hello...\n");
+        iCnt++;
+    }
+    while(iCnt <= 4);
+
+    return 0;
+}
